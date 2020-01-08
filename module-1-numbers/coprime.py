42916c73e367c51fd6a2c90aa35b52df0b329791
Mark Lindeman <m.lindeman@picturae.com>
2020-01-08 17:00:08 +0100

    Simplifies prime testing algortithm

--- module-1-numbers/coprime.py
+++ module-1-numbers/coprime.py
@@ -38,21 +38,14 @@ def get_primes(n_max):
 
     while number < n_max:
         number = number + 1
-        if (number == 2 or number % 2 != 0): #test only uneven numbers and 2
-            count = 0 #Keep track of itteration count, need this to see if we tested all combinations
+        z = 0
+        for i in range(2, number):
+            if (number % i) == 0:
+                # not a prime
+                z = z + 1
 
-            #loop thru all integers using steps of 2, no need to test even numbers
-            for x in range(3, number, 2):
-                if (number % x) == 0:
-                    #Number {number} is not a prime
-                    break
-
-                #increase counter by 2 because of loop steps is also 2:
-                count = count + 2
-
-            if (count + 3 == number or number == 2):
-                #Number is a prime so add one to primeCount
-                primes.append(number)
+        if z == 0:
+            primes.append(number)
     
     return primes
     
--- module-1-numbers/prime.py
+++ module-1-numbers/prime.py
@@ -11,20 +11,13 @@ number = 1 #Keep track of current number testing for prime
 
 while primeCount < nthPrime:
     number = number + 1
-    if (number == 2 or number % 2 != 0): #test only uneven numbers and 2
-        count = 0 #Keep track of itteration count, need this to see if we tested all combinations
+    z = 0
+    for i in range(2, number):
+        if (number % i) == 0:
+            # not a prime
+            z = z + 1
 
-        #loop thru all integers using steps of 2, no need to test even numbers
-        for x in range(3, number, 2):
-            if (number % x) == 0:
-                #Number {number} is not a prime
-                break
-
-            #increase counter by 2 because of loop steps is also 2:
-            count = count + 2
-
-        if (count + 3 == number or number == 2):
-            #Number is a prime so add one to primeCount
-            primeCount = primeCount + 1
+    if z == 0:
+        primeCount = primeCount + 1
 
 print(f"{number}")
